adc1052111ceb6b4e753be5006be152759335316
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-08-20 19:32:49 -0400

    3306

--- config/application.php
+++ config/application.php
@@ -106,7 +106,7 @@ define('WP_CONTENT_URL', WP_HOME . CONTENT_DIR);
 define('DB_NAME', env('DB_NAME'));
 define('DB_USER', env('DB_USER'));
 define('DB_PASSWORD', env('DB_PASSWORD'));
-define('DB_HOST', env('DB_HOST') ?: 'localhost');
+define( 'DB_HOST', '127.0.0.1:3306' );
 define('DB_CHARSET', 'utf8mb4');
 define('DB_COLLATE', '');
 $table_prefix = env('DB_PREFIX') ?: 'wp_';
