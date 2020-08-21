3b1a00db5ec233be230f4909157df5184065b2b8
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-08-21 11:25:33 -0400

    simple edit

--- web/wp-config.php
+++ web/wp-config.php
@@ -6,8 +6,10 @@
  */
 define('DISALLOW_FILE_MODS', false);
 define('DISALLOW_FILE_EDIT', false);
-define( 'UPLOADS', 'wp-content/uploads' );
+
  
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 require_once(dirname(__DIR__) . '/config/application.php');
 require_once(ABSPATH . 'wp-settings.php');
+
+define( 'UPLOADS', 'wp-content/uploads' );
