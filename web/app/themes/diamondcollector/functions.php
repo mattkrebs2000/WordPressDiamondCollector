30d68ea391a8fbaf7a24b20085b0d77d156fe3bc
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-08-21 16:04:12 -0400

    fix code

--- web/app/themes/diamondcollector/functions.php
+++ web/app/themes/diamondcollector/functions.php
@@ -3,94 +3,10 @@ function add_js_functions(){
 ?>
 <script>
 jQuery(document).ready(function() {
-    let givenSum = 0;
-    let currentTotal = 0;
-    let wins = 0;
-    let losses = 0;
+    var givenSum = 0;
+    var wins = 0;
+    var losses = 0;
 
-    const middle3 = () => {
-        currentTotal = 0;
-        document.getElementById("total").innerHTML = " " + currentTotal;
-    };
-
-    const middle = () => {
-        document.getElementById("total").textContent = " " + currentTotal;
-    };
-
-    const middleW = () => {
-        document.getElementById("wins").textContent = " " + wins;
-    };
-
-    const middleL = () => {
-        document.getElementById("losses").textContent = " " + losses;
-    };
-
-    const gameReset = () => {
-        redValue = 0;
-        blueValue = 0;
-        greenValue = 0;
-        purpleValue = 0;
-        setTimeout(twoSeconds, 1000 * 0.1);
-        setTimeout(middle3, 1000 * 2);
-        setTimeout(threeSeconds, 1000 * 2.5);
-    };
-
-    const win = () => {
-        document.getElementById("total").textContent = " " + currentTotal;
-        if (givenSum === currentTotal && givenSum > 0) {
-            wins++;
-            setTimeout(twoSecondsW, 1000 * 0.1);
-            setTimeout(middleW, 1000 * 1);
-            setTimeout(threeSecondsW, 1000 * 3);
-            jQuery("#begin").removeClass("begin");
-            jQuery("#lead").removeClass("margin");
-            jQuery("#container2").addClass("container2");
-            jQuery("#wrapper").addClass("wrapper");
-            jQuery("#wrapper2").addClass("wrapper2");
-            jQuery("#PurpleDiamond").addClass("imageGone");
-            jQuery("#purpleDiamond").addClass("hidden");
-            jQuery("#purpleDiamond").removeClass("animate");
-            jQuery("#RedDiamond").addClass("imageGone");
-            jQuery("#redDiamond").addClass("hidden");
-            jQuery("#redDiamond").removeClass("animate");
-            jQuery("#BlueDiamond").addClass("imageGone");
-            jQuery("#blueDiamond").addClass("hidden");
-            jQuery("#blueDiamond").removeClass("animate");
-            jQuery("#GreenDiamond").addClass("imageGone");
-            jQuery("#greenDiamond").addClass("hidden");
-            jQuery("#greenDiamond").removeClass("animate");
-            gameReset();
-        }
-    };
-
-    const loss = () => {
-        document.getElementById("total").textContent = " " + currentTotal;
-        if (currentTotal > givenSum) {
-            losses++;
-            setTimeout(twoSecondsL, 1000 * 0.1);
-            setTimeout(middleL, 1000 * 1);
-            setTimeout(threeSecondsL, 1000 * 1.5);
-            jQuery("#begin").removeClass("begin");
-            jQuery("#lead").removeClass("margin");
-            jQuery("#container2").addClass("container2");
-            jQuery("#wrapper3").addClass("wrapper3");
-            jQuery("#PurpleDiamond").addClass("imageGone");
-            jQuery("#purpleDiamond").addClass("hidden");
-            jQuery("#purpleDiamond").removeClass("animate");
-            jQuery("#RedDiamond").addClass("imageGone");
-            jQuery("#redDiamond").addClass("hidden");
-            jQuery("#redDiamond").removeClass("animate");
-            jQuery("#BlueDiamond").addClass("imageGone");
-            jQuery("#blueDiamond").addClass("hidden");
-            jQuery("#blueDiamond").removeClass("animate");
-            jQuery("#GreenDiamond").addClass("imageGone");
-            jQuery("#greenDiamond").addClass("hidden");
-            jQuery("#greenDiamond").removeClass("animate");
-            console.log("currentTotal" + currentTotal);
-            console.log("givenSum" + givenSum);
-            gameReset();
-        }
-    };
 
     const changeButton = () => {
         document.getElementById("begin").innerHTML = "Play Again!";
@@ -134,11 +50,97 @@ jQuery(document).ready(function() {
     jQuery("#greenDiamond").removeClass("hidden");
     jQuery("#greenDiamond").addClass("animate");
 
+
+
     jQuery("#begin").on("click", function() {
+        const middleW = () => {
+            document.getElementById("wins").textContent = " " + wins;
+        };
+
+        const win = () => {
+            document.getElementById("total").textContent = " " + currentTotal;
+            if (givenSum === currentTotal && givenSum > 0) {
+                wins = wins + 1;
+                setTimeout(twoSecondsW, 1000 * 0.1);
+                setTimeout(middleW, 1000 * 1);
+                setTimeout(threeSecondsW, 1000 * 3);
+                jQuery("#begin").removeClass("begin");
+                jQuery("#lead").removeClass("margin");
+                jQuery("#container2").addClass("container2");
+                jQuery("#wrapper").addClass("wrapper");
+                jQuery("#wrapper2").addClass("wrapper2");
+                jQuery("#PurpleDiamond").addClass("imageGone");
+                jQuery("#purpleDiamond").addClass("hidden");
+                jQuery("#purpleDiamond").removeClass("animate");
+                jQuery("#RedDiamond").addClass("imageGone");
+                jQuery("#redDiamond").addClass("hidden");
+                jQuery("#redDiamond").removeClass("animate");
+                jQuery("#BlueDiamond").addClass("imageGone");
+                jQuery("#blueDiamond").addClass("hidden");
+                jQuery("#blueDiamond").removeClass("animate");
+                jQuery("#GreenDiamond").addClass("imageGone");
+                jQuery("#greenDiamond").addClass("hidden");
+                jQuery("#greenDiamond").removeClass("animate");
+                gameReset();
+            }
+        };
+
+        const middleL = () => {
+            document.getElementById("losses").textContent = " " + losses;
+        };
+
+        const loss = () => {
+            document.getElementById("total").textContent = " " + currentTotal;
+            if (currentTotal > givenSum) {
+                losses = losses + 1;
+                setTimeout(twoSecondsL, 1000 * 0.1);
+                setTimeout(middleL, 1000 * 1);
+                setTimeout(threeSecondsL, 1000 * 1.5);
+                jQuery("#begin").removeClass("begin");
+                jQuery("#lead").removeClass("margin");
+                jQuery("#container2").addClass("container2");
+                jQuery("#wrapper3").addClass("wrapper3");
+                jQuery("#PurpleDiamond").addClass("imageGone");
+                jQuery("#purpleDiamond").addClass("hidden");
+                jQuery("#purpleDiamond").removeClass("animate");
+                jQuery("#RedDiamond").addClass("imageGone");
+                jQuery("#redDiamond").addClass("hidden");
+                jQuery("#redDiamond").removeClass("animate");
+                jQuery("#BlueDiamond").addClass("imageGone");
+                jQuery("#blueDiamond").addClass("hidden");
+                jQuery("#blueDiamond").removeClass("animate");
+                jQuery("#GreenDiamond").addClass("imageGone");
+                jQuery("#greenDiamond").addClass("hidden");
+                jQuery("#greenDiamond").removeClass("animate");
+                console.log("currentTotal" + currentTotal);
+                console.log("givenSum" + givenSum);
+                gameReset();
+            }
+        };
+
+        const middle3 = () => {
+            currentTotal = 0;
+            document.getElementById("total").innerHTML = " " + currentTotal;
+        }
+
+        const gameReset = () => {
+            redValue = 0;
+            blueValue = 0;
+            greenValue = 0;
+            purpleValue = 0;
+            setTimeout(twoSeconds, 1000 * 0.1);
+            setTimeout(middle3, 1000 * 2);
+            setTimeout(threeSeconds, 1000 * 2.5);
+        };
+
+        const middle = () => {
+            document.getElementById("total").textContent = " " + currentTotal;
+        }
+
         changeButton();
         win();
         loss();
-        let currentTotal = 0;
+        var currentTotal = 0;
         document.getElementById("total").innerHTML = currentTotal;
         jQuery("#begin").addClass("begin");
         jQuery("#lead").addClass("margin");
@@ -160,20 +162,20 @@ jQuery(document).ready(function() {
         jQuery("#greenDiamond").removeClass("animate");
         document.getElementById("givenSum").textContent = " " + givenSum;
         document.getElementById("total").textContent = " " + currentTotal;
-        let total = Math.floor(Math.random() * 12 + 1) + 19;
+        var total = Math.floor(Math.random() * 12 + 1) + 19;
         console.log("total" + total);
-        let numbersArrrayForDiamonds = [];
-        for (let i = 0; i < 4; i++) {
-            let newNumber = Math.floor(Math.random() * 12 + 1);
+        var numbersArrrayForDiamonds = [];
+        for (var i = 0; i < 4; i++) {
+            var newNumber = Math.floor(Math.random() * 12 + 1);
             if (!numbersArrrayForDiamonds.includes(newNumber)) {
                 numbersArrrayForDiamonds.push(newNumber);
             } else {
                 i--;
             }
         }
-        let arrayForIndexOfNumberFindingArray = [];
-        for (let j = 0; j < 4; j++) {
-            let indexOfNumberFindingArray = Math.floor(Math.random() * 4 + 1);
+        var arrayForIndexOfNumberFindingArray = [];
+        for (var j = 0; j < 4; j++) {
+            var indexOfNumberFindingArray = Math.floor(Math.random() * 4 + 1);
             if (
                 !arrayForIndexOfNumberFindingArray.includes(indexOfNumberFindingArray)
             ) {
@@ -182,21 +184,21 @@ jQuery(document).ready(function() {
                 j--;
             }
         }
-        let purpleMultiplier = arrayForIndexOfNumberFindingArray[0];
-        let redMultiplier = arrayForIndexOfNumberFindingArray[1];
-        let blueMultiplier = arrayForIndexOfNumberFindingArray[2];
-        let greenMultiplier = arrayForIndexOfNumberFindingArray[3];
-        let purpleValue = numbersArrrayForDiamonds[0];
-        let redValue = numbersArrrayForDiamonds[1];
-        let blueValue = numbersArrrayForDiamonds[2];
-        let greenValue = numbersArrrayForDiamonds[3];
-        let purpleProduct = purpleMultiplier * purpleValue;
-        let redProduct = redMultiplier * redValue;
-        let blueProduct = blueMultiplier * blueValue;
-        let greenProduct = greenMultiplier * greenValue;
+        var purpleMultiplier = arrayForIndexOfNumberFindingArray[0];
+        var redMultiplier = arrayForIndexOfNumberFindingArray[1];
+        var blueMultiplier = arrayForIndexOfNumberFindingArray[2];
+        var greenMultiplier = arrayForIndexOfNumberFindingArray[3];
+        var purpleValue = numbersArrrayForDiamonds[0];
+        var redValue = numbersArrrayForDiamonds[1];
+        var blueValue = numbersArrrayForDiamonds[2];
+        var greenValue = numbersArrrayForDiamonds[3];
+        var purpleProduct = purpleMultiplier * purpleValue;
+        var redProduct = redMultiplier * redValue;
+        var blueProduct = blueMultiplier * blueValue;
+        var greenProduct = greenMultiplier * greenValue;
         givenSum = purpleProduct + redProduct + blueProduct + greenProduct;
         console.log("givenSUM" + givenSum);
-        let sumLocation = document.getElementById("givenSum");
+        var sumLocation = document.getElementById("givenSum");
         sumLocation.textContent = " " + givenSum;
 
         jQuery("#PurpleDiamond").on("click", function() {
@@ -207,9 +209,11 @@ jQuery(document).ready(function() {
             jQuery("#purpleDiamond").addClass("animate");
             console.log("purpleValue= " + purpleValue);
             currentTotal = currentTotal += purpleValue;
+
             setTimeout(twoSeconds, 1000 * 0.1);
             setTimeout(middle, 1000 * 1);
             setTimeout(threeSeconds, 1000 * 1.5);
+
             jQuery("#RedDiamond").removeClass("imageGone");
             jQuery("#redDiamond").addClass("hidden");
             jQuery("#redDiamond").removeClass("animate");
@@ -219,7 +223,6 @@ jQuery(document).ready(function() {
             jQuery("#GreenDiamond").removeClass("imageGone");
             jQuery("#greenDiamond").addClass("hidden");
             jQuery("#greenDiamond").removeClass("animate");
-
             win();
             loss();
         });
@@ -231,6 +234,10 @@ jQuery(document).ready(function() {
             jQuery("#redDiamond").removeClass("hidden");
             jQuery("#redDiamond").addClass("animate");
             currentTotal = currentTotal += redValue;
+
+            function middle() {
+                document.getElementById("total").textContent = " " + currentTotal;
+            }
             setTimeout(twoSeconds, 1000 * 0.1);
             setTimeout(middle, 1000 * 1);
             setTimeout(threeSeconds, 1000 * 1.5);
@@ -254,6 +261,10 @@ jQuery(document).ready(function() {
             jQuery("#blueDiamond").removeClass("hidden");
             jQuery("#blueDiamond").addClass("animate");
             currentTotal = currentTotal += blueValue;
+
+            function middle() {
+                document.getElementById("total").textContent = " " + currentTotal;
+            }
             setTimeout(twoSeconds, 1000 * 0.1);
             setTimeout(middle, 1000 * 1);
             setTimeout(threeSeconds, 1000 * 1.5);
@@ -277,6 +288,10 @@ jQuery(document).ready(function() {
             jQuery("#greenDiamond").removeClass("hidden");
             jQuery("#greenDiamond").addClass("animate");
             currentTotal = currentTotal += greenValue;
+
+            function middle() {
+                document.getElementById("total").textContent = " " + currentTotal;
+            }
             setTimeout(twoSeconds, 1000 * 0.1);
             setTimeout(middle, 1000 * 1);
             setTimeout(threeSeconds, 1000 * 1.5);
@@ -298,6 +313,10 @@ jQuery(document).ready(function() {
             jQuery("#lead").addClass("margin");
             console.log("purpleValue= " + purpleValue);
             currentTotal = currentTotal += purpleValue;
+
+            function middle() {
+                document.getElementById("total").textContent = " " + currentTotal;
+            }
             setTimeout(twoSeconds, 1000 * 0.1);
             setTimeout(middle, 1000 * 1);
             setTimeout(threeSeconds, 1000 * 1.5);
@@ -318,6 +337,10 @@ jQuery(document).ready(function() {
             jQuery("#begin").addClass("begin");
             jQuery("#lead").addClass("margin");
             currentTotal = currentTotal += redValue;
+
+            function middle() {
+                document.getElementById("total").textContent = " " + currentTotal;
+            }
             setTimeout(twoSeconds, 1000 * 0.1);
             setTimeout(middle, 1000 * 1);
             setTimeout(threeSeconds, 1000 * 1.5);
@@ -338,6 +361,10 @@ jQuery(document).ready(function() {
             jQuery("#begin").addClass("begin");
             jQuery("#lead").addClass("margin");
             currentTotal = currentTotal += blueValue;
+
+            function middle() {
+                document.getElementById("total").textContent = " " + currentTotal;
+            }
             setTimeout(twoSeconds, 1000 * 0.1);
             setTimeout(middle, 1000 * 1);
             setTimeout(threeSeconds, 1000 * 1.5);
@@ -358,6 +385,10 @@ jQuery(document).ready(function() {
             jQuery("#begin").addClass("begin");
             jQuery("#lead").addClass("margin");
             currentTotal = currentTotal += greenValue;
+
+            function middle() {
+                document.getElementById("total").textContent = " " + currentTotal;
+            }
             setTimeout(twoSeconds, 1000 * 0.1);
             setTimeout(middle, 1000 * 1);
             setTimeout(threeSeconds, 1000 * 1.5);
@@ -402,19 +433,9 @@ jQuery(document).ready(function() {
             jQuery("#greenDiamond").addClass("hidden");
             jQuery("#greenDiamond").removeClass("animate");
         });
-        console.log("blueValue" + blueValue);
-        console.log("redValue" + redValue);
-        console.log("purple value" + purpleValue);
-        console.log("green value" + greenValue);
-        console.log("currentTotal" + currentTotal);
-        console.trace("currentTotal" + currentTotal);
 
-        // console.log(numbersArrrayForDiamonds)
-        // console.log(arrayForIndexOfNumberFindingArray);
     });
 
-    console.trace("currentTotal" + currentTotal);
-    console.log("givenSum" + givenSum);
 });
 </script>
 <?php
